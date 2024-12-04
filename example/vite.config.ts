858c69ebc60c46feb9f5a22d1be94ea3de9325a1
Javier Donoso <javier.donoso@nuilea.com>
2024-12-04 18:34:40 +0100

    social:login

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,31 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(),  nodePolyfills()],
+  resolve: {
+    alias: {
+      crypto: "empty-module",
+      assert: "empty-module",
+      http: "empty-module",
+      https: "empty-module",
+      os: "empty-module",
+      url: "empty-module",
+      zlib: "empty-module",
+      stream: "stream",
+      _stream_duplex: "empty-module",
+      _stream_passthrough: "empty-module",
+      _stream_readable: "empty-module",
+      _stream_writable: "empty-module",
+      _stream_transform: "empty-module",
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  define: {
+    global: "globalThis",
+  },
+});
